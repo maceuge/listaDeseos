434c6715a4412ed37d65b95feee17d278ebe1a2c
maceuge <maceuge@icloud.com>
2018-07-17 18:08:40 -0300

    Creacion de componentes (Agregar, Pendientes y Terminados), Ajustes del Diseño y creacion de lista y sus items

--- src/pages/agregar/agregar.component.ts
+++ src/pages/agregar/agregar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DeseosService } from '../../services/deseos.service';
 import { Lista } from '../../models/lista.model';
+import { NavParams } from 'ionic-angular';
+import { ListaItem } from '../../models';
 
 @Component({
     selector: 'app-agregar',
@@ -9,13 +11,33 @@ import { Lista } from '../../models/lista.model';
 })
 export class AgregarComponent implements OnInit {
 
-    listas: Lista[] = [];
+    lista: Lista;
+    nombreItem: string = '';
     
-    constructor(public deseosService: DeseosService) {
-       
+    constructor(public deseosService: DeseosService,
+                private navParams: NavParams) {
+
+        let title = this.navParams.get('titulo');
+        this.lista = new Lista(title);             
+    
      }
 
     ngOnInit(): void {}
 
+    guardarItem() {
+       if (this.nombreItem.length === 0) {
+           return;
+       }
+       const nuevoItem = new ListaItem(this.nombreItem);
+       this.lista.item.push(nuevoItem);
+       this.nombreItem = '';
+    }
 
+    checkItem(item: ListaItem) {
+        item.completado = !item.completado;
+    }
+
+    eliminarItem(idx: number) {
+        this.lista.item.splice(idx, 1);
+    }
 }
--- src/pages/pendientes/pendientes.component.ts
+++ src/pages/pendientes/pendientes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DeseosService } from '../../services/deseos.service';
 import { Lista } from '../../models/lista.model';
+import { NavController, AlertController } from 'ionic-angular';
+import { AgregarComponent } from '../agregar/agregar.component';
 
 @Component({
     selector: 'app-pendientes',
@@ -11,10 +13,11 @@ export class PendienteComponent implements OnInit {
 
     listas: Lista[] = [];
     
-    constructor(public deseosService: DeseosService) {
+    constructor(public deseosService: DeseosService,
+                private navCtrl: NavController,
+                private alertCtrl: AlertController) {
 
         this.listas = this.deseosService.getListas();
-
      }
 
     ngOnInit(): void { }
@@ -22,4 +25,43 @@ export class PendienteComponent implements OnInit {
     listaSelected (item: Lista) {
         console.log(item);
     }
+
+    agregarLista() {
+        this.navCtrl.push(AgregarComponent);
+    }
+
+    showPrompt() {
+        const prompt = this.alertCtrl.create({
+          title: 'Agregar Tarea',
+          message: "Escriba el nombre de la tarea que quiere agregar",
+          inputs: [
+            {
+              name: 'titulo',
+              placeholder: 'Tarea'
+            },
+          ],
+          buttons: [
+            {
+              text: 'Cancelar',
+              handler: data => {
+                console.log('Cancelar clicked');
+              }
+            },
+            {
+              text: 'Guardar',
+              handler: data => {
+                if (data.titulo.lenght === 0) {
+                    return;
+                }
+                this.navCtrl.push(AgregarComponent, {
+                    titulo: data.titulo
+                });
+              }
+            }
+          ]
+        });
+        prompt.present();
+    }
+    
+
 }
